cb1fcf1f243847b0df9ac3a06a3e8abc7478d952
Brandon <brandon.p@prhsrobotics.com>
2025-01-21 01:10:34 -0500

    update

--- DATA/Patient/Patient_Info.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE results(
-    id INT PRIMARY KEY ,
-    patient_id INT,
-    test_name VARCHAR(100),
-    result_value VARCHAR(100),
-    FOREIGN KEY (patient_id) REFERENCES patients(id)
-)
