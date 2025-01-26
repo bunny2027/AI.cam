90d299f2664af53664cde1b224ab241afc15b3cc
Brandon <brandon.p@prhsrobotics.com>
2025-01-26 10:20:28 -0500

    update

--- Interface/interfacefunc.js
+++ Interface/interfacefunc.js
@@ -1,3 +1,12 @@
-export function greetUser(name) {
-    console.log('Hello, ${name}! Welcome to Ascelpius.');
-}
+const signUpButton = document.getElementById('signup');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click',() => {
+    container.classList.add("right-planel-activate");
+});
+
+signInButton.addEventListener('click',() => {
+    container.classList.remove("right-planel-activate");
+});
+
